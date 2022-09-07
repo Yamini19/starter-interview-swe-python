c8d5987e1b02aa742d8e39e449e37f941cffd9d8
Yamini19 <yamini.punetha@gmail.com>
2022-09-08 00:48:16 +0530

    Added checks in post and implemented GET

--- db.py
+++ db.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Reading:
     # TODO: change this to represent whatever information is needed
     def __init__(self, time, name, value) -> None:
@@ -5,22 +8,28 @@ class Reading:
         self.name = name
         self.value = value
 
-
+    def to_json(self):
+        return {
+            "time": self.time,
+            "name": self.name,
+            "value": self.value
+        }
 
 # This is a fake database which stores data in-memory while the process is running
 # Feel free to change the data structure to anything else you would like
-database: dict[str, Reading] = {}
+database: list[Reading] = []
 
 
-def add_reading(key: str, reading: Reading) -> None:
+def add_reading(reading: Reading) -> None:
     """
     Store a reading in the database using the given key
     """
-    database[key] = reading
+    database.append(reading)
 
 
-def get_reading(key: str) -> Reading | None:
+def get_reading(fromDate: datetime.date, toDate: datetime.date) -> Reading | None:
     """
     Retrieve a reading from the database using the given key
     """
-    return database.get(key)
+    filteredRecords = [ record for record in database if (record.time.date() >=fromDate) and (record.time.date() <=toDate)]
+    return filteredRecords
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from curses import raw
+import datetime
 import io
 import pandas as pd
 from flask import Flask, jsonify, request
@@ -14,23 +14,53 @@ def post_data():
     #  {timestamp} {name} {value}
 
     data = request.data
-    column_names=["TimeStamp","Name",'Value']
-    rawData = pd.DataFrame(pd.read_csv(io.StringIO(data.decode('utf-8')), names=column_names, sep= " "))
-    rawData['Time'] = pd.to_datetime(rawData['TimeStamp'], 
-                                  unit='s')
+    column_names=["Time","Name",'Value']
+    
+    try: 
+        rawData = pd.DataFrame(pd.read_csv(io.StringIO(data.decode('utf-8')), names=column_names, sep= " ",index_col= False))
+        rawData['Time'] = pd.to_datetime(rawData['Time'], unit='s')
+        rawData.astype({'Value': 'float'}).dtypes
+        type(rawData.shape)
+    except pd.errors.ParserError as error:
+        return jsonify({ "success":  'false'})
+    except ValueError as error:
+        return jsonify({ "success":  'false'})
+
 
     for ind in rawData.index:
-        print(rawData['Time'][ind], rawData['Name'][ind], rawData['Value'][ind])
         reading = Reading(rawData['Time'][ind], rawData['Name'][ind], rawData['Value'][ind])
-        add_reading(str(rawData['TimeStamp'][ind]), reading)
+        add_reading(reading)
     return jsonify({ "success": "true" })
 
 
 @app.route("/data", methods=['GET'])
 def get_data():
     # TODO: check what dates have been requested, and retrieve all data within the given range
-    get_reading("1649941817")
-    return jsonify({ "success": "true" })
+    fromDate = request.args.get('from')
+    toDate = request.args.get('to')
+
+    if fromDate is None or fromDate == '""':
+        return jsonify({ "message": "Please provide a valid From Date" })
+    if toDate is None or toDate == '""':
+        return jsonify({ "message": "Please provide a valid To Date" })
+    
+    try:
+        fromDate= datetime.datetime.strptime(fromDate, '%Y-%m-%d').date()
+        toDate= datetime.datetime.strptime(toDate, "%Y-%m-%d").date()
+    except ValueError as error: 
+        return jsonify({ "message":  str(error)})
+    
+    if toDate < fromDate:
+        return jsonify({ "message": "parameter TO should be greater or equal to FROM" })
+
+    response = []
+
+    filteredReading = get_reading(fromDate, toDate)
+
+    for reading in filteredReading: 
+        response.append(Reading(str(reading.time),reading.name,reading.value).to_json())
+    
+    return jsonify(response)
 
 
 if __name__ == "__main__":
